8d6413f6157c562547eaf43f97d53abd69a27ff1
rshajgude <rshajgude@gmail.com>
2026-07-10 11:34:13 +0530

    If condition

---
+++ 05_IF_condition/if_condition.py
@@ -0,0 +1,13 @@
+
+n=input("Enter a number of your choice : ")
+print(n)
+n=int(n)
+
+if n%2==0:
+    print("Number is an even")
+else:
+    print("Number is an odd")
+
+#message="Number is an even" if n%2==2 else "Number is an odd"
+#print(message)
+
