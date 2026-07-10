dde0e67333e10e4f66b170037fb927f3cf8dfb47
rshajgude <rshajgude@gmail.com>
2026-07-10 11:44:15 +0530

    If condition elif

--- 05_IF_condition/if_condition.py
+++ 05_IF_condition/if_condition.py
@@ -11,3 +11,16 @@ else:
 #message="Number is an even" if n%2==2 else "Number is an odd"
 #print(message)
 
+marks=45
+
+print(f"You got {marks} marks and ..")
+if marks>75:
+    print("You got distinction")
+elif marks>65:
+    print("You got first class")
+elif marks>50:
+    print("You are passed with second class")
+elif marks>35:
+    print("You are passed")
+else:
+    print("You are failed..!")
